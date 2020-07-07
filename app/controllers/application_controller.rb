3e502cd21b0030aebf12e771f2172aa69773eef0
naoki-tsunekawa <b1014138@gmail.com>
2020-07-08 00:53:17 +0900

    ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  # 全てのコントローラーからセッションに保管されているログイン情報を利用できるようにする
+  # helper_method :current_user
+  before_action :current_user
+
+
+  private
+
+  def current_user
+    # セッションにログイン情報がある場合、@current_userにユーザ情報を格納する
+    if session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+    end 
+  end
 end
